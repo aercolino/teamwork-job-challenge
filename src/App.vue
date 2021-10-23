a6654c9ff41418bd83d82bdbfd6bfd53bd9dc5c1
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2021-10-23 11:27:08 +0200

    Add a basic "PeopleList" component; show "PeopleList"; remove "HelloWorld" component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" style="height: 40px;">
+    <PeopleList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PeopleList from './components/PeopleList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PeopleList
   }
 }
 </script>
---
+++ src/components/PeopleList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>List of Star Wars Peolple</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'PeopleList',
+  data() {
+    return {}
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
