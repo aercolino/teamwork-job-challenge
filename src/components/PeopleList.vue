cc1f2a81f5b4650753b33523d772affb171096f7
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2021-10-23 11:55:24 +0200

    Add fake list of people; add basic "PlanetPopup" component; show "PlanetPopup"

--- src/components/PeopleList.vue
+++ src/components/PeopleList.vue
@@ -1,20 +1,82 @@
 <template>
-  <div>
+  <div id="PeopleList">
     <h1>List of Star Wars Peolple</h1>
+
+    <table width="100%">
+      <tr>
+        <th width="20%">
+          Name
+        </th>
+        <th width="10%">
+          Height
+        </th>
+        <th width="10%">
+          Mass
+        </th>
+        <th width="20%">
+          Created
+        </th>
+        <th width="20%">
+          Edited
+        </th>
+        <th width="20%">
+          Planet Name
+        </th>
+      </tr>
+      <tr v-for="i in rows" :key="i">
+        <td>
+          Luke Skywalker
+        </td>
+        <td>
+          172
+        </td>
+        <td>
+          77
+        </td>
+        <td>
+          2014-12-09T13:50:51.644000Z
+        </td>
+        <td>
+          2014-12-20T21:17:56.891000Z
+        </td>
+        <td>
+          <PlanetPopup id="1"/>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 
 <script>
+import PlanetPopup from './PlanetPopup';
+
 export default {
   name: 'PeopleList',
+  components: {
+    PlanetPopup,
+  },
   data() {
-    return {}
+    return {
+      rows: Array(10).fill(null),
+    }
   }
 }
 </script>
 
 
 <style scoped>
+#PeopleList {
+  font-size: 14px;
+}
 
+table {
+  border-collapse: collapse;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 14px 21px;
+}
 </style>
---
+++ src/components/PlanetPopup.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Tattoine
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: ['id'],
+}
+</script>
+
+
+<style scoped>
+
+</style>
