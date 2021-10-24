e70cc3fe513e4af4fbda36be79f67567114cef2f
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2021-10-24 14:01:16 +0200

    Add basic "Modal" component

--- src/components/PlanetPopup.vue
+++ src/components/PlanetPopup.vue
@@ -1,28 +1,57 @@
 <template>
   <div>
     {{ planetName }}
+    <button
+      type="button"
+      class="btn"
+      @click="showModal"
+    >
+      Open Modal!
+    </button>
+
+    <Modal
+      v-show="isModalVisible"
+      @close="closeModal"
+    />
   </div>
 </template>
 
 
 <script>
+import Modal from "./Modal.vue";
+
 export default {
-  props: ['id'],
+  props: ["id"],
+  components: {
+    Modal,
+  },
+  data() {
+    return {
+      isModalVisible: false,
+    };
+  },
   computed: {
     planet() {
       return this.$store.state.planets[this.id];
     },
     planetName() {
-      return this.planet?.name ?? 'Loading...';
-    }
+      return this.planet?.name ?? "Loading...";
+    },
+  },
+  methods: {
+    showModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
   },
   mounted() {
-    this.$store.dispatch('loadPlanetIfMissing', {id: this.id});
+    this.$store.dispatch("loadPlanetIfMissing", { id: this.id });
   },
-}
+};
 </script>
 
 
 <style scoped>
-
 </style>
