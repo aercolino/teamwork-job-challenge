2f0892526a5a4d7fd2cd25e92a45cffda316fbc1
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2021-10-25 10:41:42 +0200

    Prefer "UTC" to "GMT"

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ Vue.config.productionTip = false;
 Vue.filter('asLogTime', function asLogTime(isoTime = '') {
   if (!isoTime) return;
   if (!/^\d\d\d\d-\d\d-\d\dT\d\d:\d\d:\d\d\.\d+Z$/.test(isoTime)) return isoTime;
-  // 2014-12-10T11:35:48.479000Z -> 2014-12-10 11:35 GMT
+  // 2014-12-10T11:35:48.479000Z -> 2014-12-10 11:35 UTC
   const upToMinutes = isoTime.substr(0, 16).replace('T', ' ');
-  return `${upToMinutes} GMT`;
+  return `${upToMinutes} UTC`;
 });
 
 new Vue({
