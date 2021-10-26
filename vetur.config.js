ac70f67b3c3c4416211b8b497c5210d6c6db59d8
Andrea Ercolino <cappuccino.e.cornetto@gmail.com>
2021-10-26 11:29:49 +0200

    Make Vetur happy (4th try)
    It's exhausting...

--- vetur.config.js
+++ vetur.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   settings: {
-    "vetur.useWorkspaceDependencies": true,
-    "vetur.experimental.templateInterpolationService": true
+    "vetur.validation.interpolation": false,
+    "vetur.ignoreProjectWarning": true,
   },
   projects: [
     "./"
